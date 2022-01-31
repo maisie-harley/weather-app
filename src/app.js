c2aa3af9c05bd8b21ea2d3f8a5b070c05fb73337
Maisie <maisie.a.harley@gmail.com>
2022-01-31 17:52:29 +0000

    Showing Weather

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "bed09b6e871276d7a5b1c881b630cf54";
 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
 
-axios.get(url).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
